a8fdf9a0a5db37edf6d94f56ae199a71e0efb896
개발바닥 아방수 <98931608+staygold94@users.noreply.github.com>
2023-09-07 23:25:31 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 2.22 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -1,16 +1,21 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(long n) {
         
-        // Long 타입을 String으로 변환하여 길이 구함
-        int[] answer = new int[Long.toString(n).length()];
-        int i = 0;
-        
-        while(n > 0) {
-            answer[i] = (int) (n % 10);
-            n /= 10;
-            i++;
-        }
-        
-        return answer;
+        // 인상 깊었던 풀이
+        return new StringBuilder().append(n).reverse().chars().map(Character::getNumericValue).toArray();
     }
 }
+
+/*
+제시된 Java 함수는 주어진 `long` 값을 역순으로 배열로 변환하는 것을 목표로 하는 것으로 보입니다. 여기에서 `chars()` 메서드로 문자열로 변환한 후, `map(Character::getNumericValue)` 메서드를 사용한 후 `toArray()`로 배열로 변환하는 이유는 다음과 같습니다:
+
+1. `chars()`: `chars()` 메서드를 사용하여 `long` 값을 문자열로 변환합니다. `StringBuilder`를 사용하여 숫자를 문자열로 변환하고, `chars()` 메서드를 호출하면 문자열의 각 문자가 순차적으로 처리 가능한 `IntStream`으로 반환됩니다.
+
+2. `map(Character::getNumericValue)`: `map` 메서드는 각 문자를 숫자 값으로 변환하기 위해 `Character::getNumericValue` 함수를 적용합니다. 이 함수는 각 문자를 해당 숫자 값으로 변환합니다.
+
+3. `toArray()`: `toArray()` 메서드는 `IntStream`의 결과를 배열로 변환합니다. 따라서 역순으로 변환된 숫자 값을 배열로 반환합니다.
+
+전체 과정을 통해 `long` 값을 역순으로 된 숫자 배열로 변환하는 것입니다. 이 함수는 주어진 `long` 값을 역순으로 배열로 변환하기 위한 것으로 보입니다.
+*/
