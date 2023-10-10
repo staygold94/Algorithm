7847eb18068ce0c7faf134a1a318ed2dc8a97caa
개발바닥 아방수 <98931608+staygold94@users.noreply.github.com>
2023-10-10 17:24:21 +0900

    [Bronze III] Title: A+B - 6, Time: 132 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<num; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(),",");
+            int answer = 0;
+
+            while(st.hasMoreTokens()) {
+                answer += Integer.parseInt(st.nextToken());
+            }
+            System.out.println(answer);
+        }
+    }
+}
