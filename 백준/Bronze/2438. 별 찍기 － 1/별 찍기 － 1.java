60b812cc1cb79fda2989ddb7b5c3af6d2009661e
개발바닥 아방수 <98931608+staygold94@users.noreply.github.com>
2023-10-12 15:05:25 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main{
+		public static void main(String[] args) throws IOException {
+
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+				int num = Integer.parseInt(br.readLine());
+
+				for (int i=1; i <= num; i++) {
+						for (int j=1; j <= i; j++) {
+								bw.write("*");
+						}
+						bw.write("\n");
+				}
+				bw.close();
+		}
+}
