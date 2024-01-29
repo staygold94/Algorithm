dcf6a808d1b6ceeba784ce67d09d5b82d3d61077
개발바닥 아방수 <98931608+staygold94@users.noreply.github.com>
2024-01-29 21:35:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 99.92 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -8,20 +8,18 @@ class Solution {
 
         // 해시맵에 참가자 명단을 넣는다.
         for(Integer i=0; i<participant.length; i++) {
-            map.put(participant[i], map.containsKey(participant[i]) ? map.get(participant[i]) + 1 : 1);
+            map.put(participant[i], i);
         }
 
         // 해시맵에서 참가자 명단을 제거한다.
         for(Integer i=0; i<completion.length; i++) {
-            if(map.get(completion[i]) > 1)
-                map.put(completion[i], map.get(completion[i]) - 1);
-            else
-                map.remove(completion[i]);
+            map.remove(completion[i]);
         }
 
         for(String key: map.keySet()) {
             answer = key;
         }
+
         return answer;
     }
 }
