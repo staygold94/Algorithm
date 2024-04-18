0f7c4389a236c6365cd85aa03064c9dafd5187ef
개발바닥 아방수 <98931608+staygold94@users.noreply.github.com>
2024-04-19 07:56:52 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,38 +1,12 @@
-import java.util.*;
-
 class Solution {
     public int solution(String s) {
-        String answer = "";
-        int idx = 0;    // substring 시작점 idx (answer에 들어가지 않은 위치 저장)
-        HashMap<String, Integer> map = new HashMap<>();
+        String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
-        map.put("zero", 0);
-        map.put("one", 1);
-        map.put("two", 2);
-        map.put("three", 3);
-        map.put("four", 4);
-        map.put("five", 5);
-        map.put("six", 6);
-        map.put("seven",7);
-        map.put("eight", 8);
-        map.put("nine", 9);
-        
-
-        for(int i=0; i<s.length(); i++) {
-            // 숫자인 경우
-            if(Character.isDigit(s.charAt(i))) {
-                answer += s.charAt(i);
-                idx = i+1;
-            // 문자인 경우
-            } else {
-                String str = s.substring(idx, i+1);
-                // map에 저장된 문자열인 경우
-                if(map.containsKey(str)) {
-                    answer += map.get(str);
-                    idx = i+1;
-                }
+        for(int i=0; i<arr.length; i++) {
+            if(s.contains(arr[i])) {
+                s = s.replace(arr[i], Integer.toString(i));
             }
         }
-        return Integer.parseInt(answer);
+        return Integer.parseInt(s);
     }
 }
