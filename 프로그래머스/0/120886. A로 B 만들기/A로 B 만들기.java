4e107dd0befdefcb7733f33ddeafd9a39d865992
개발바닥 아방수 <98931608+staygold94@users.noreply.github.com>
2024-05-25 14:45:38 +0900

    [level 0] Title: A로 B 만들기, Time: 0.38 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -2,25 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(String before, String after) {
-        int answer = 1;
-        
-        HashMap<Character, Integer> map = new HashMap<>();
-        
-        for(int i=0; i<before.length(); i++) {
-            map.put(before.charAt(i), map.getOrDefault(before.charAt(i), 0) + 1);
-        }
 
-        for(int i=0; i<after.length(); i++) {
-            map.put(after.charAt(i), map.getOrDefault(after.charAt(i), 0) - 1);
-        }
+        char[] beforeArr = before.toCharArray();
+        char[] afterArr = after.toCharArray();
 
-        for(char ch : map.keySet()) {
-            if(map.get(ch) != 0) {
-                answer = 0;
-                break;
-            }
-        }
-        
-        return answer;
+        Arrays.sort(beforeArr);
+        Arrays.sort(afterArr);
+
+        return new String(beforeArr).equals(new String(afterArr)) ? 1 : 0;
     }
 }
