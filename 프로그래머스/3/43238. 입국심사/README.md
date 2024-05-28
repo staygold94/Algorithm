e3a6046911814f80273308a641a8f4166db2dd8b
개발바닥 아방수 <98931608+staygold94@users.noreply.github.com>
2024-05-28 10:52:24 +0900

    [level 3] Title: 입국심사, Time: 0.37 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 75.6 MB, 시간: 0.35 ms
+메모리: 82.7 MB, 시간: 0.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 23일 12:58:04
+2024년 05월 28일 10:52:23
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,33 +1,32 @@
 import java.util.*;
 
 class Solution {
-    // 이분탐색을 통해 최솟값 구하기
     public long solution(int n, int[] times) {
-        long answer = 0;
-        Arrays.sort(times); // 정렬
 
-        // 시작점과 끝점 지정 (끝점 = 사람 수 * 오래 걸리는 심사대)
-        long start = 0;
-        long end = (long)n * times[times.length-1];
+        Arrays.sort(times);
 
-        while(start <= end) {
-            // 중간값
-            long mid = (start + end) / 2;
-            // 검사 수
-            long count = 0;
+        long left = 1;
+        long right = (long) n * times[times.length - 1]; // 가장 최대치
+
+        // left < right 조건
+        while(left < right) {
+            long mid = (left + right) / 2;  // 중간 초
+            long count = 0;                 // 심사 가능한 사람 수
+
+            // 반복문으로 심사 가능한 사람 수 계산
             for(int i=0; i<times.length; i++) {
                 count += mid / times[i];
             }
-            // 검사수가 부족한 경우
-            if(count < n) {
-                start = mid + 1;
+            
+            // 필요 인원 초과시
+            if(count >= n) {
+                right = mid;
 
-            // 검사수가 차는 경우
+            // 필요인원 이하인 경우
             } else {
-                end = mid - 1;
-                answer = mid;   // 더 최솟값이 있을 수 있다
+                left = mid + 1;
             }
         }
-        return answer;
+        return left;
     }
 }
