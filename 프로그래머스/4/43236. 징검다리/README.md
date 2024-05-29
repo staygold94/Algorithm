c38f879833edbe528f2b6bf0a5be95e1eadadb85
개발바닥 아방수 <98931608+staygold94@users.noreply.github.com>
2024-05-29 15:40:06 +0900

    [level 4] Title: 징검다리, Time: 23.14 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/README.md
+++ 프로그래머스/4/43236. 징검다리/README.md
@@ -1,10 +1,10 @@
 # [level 4] 징검다리 - 43236 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43236?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43236) 
 
 ### 성능 요약
 
-메모리: 84.1 MB, 시간: 21.01 ms
+메모리: 80.7 MB, 시간: 23.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 25일 21:58:25
+2024년 05월 29일 15:40:03
 
 ### 문제 설명
 
--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -4,45 +4,38 @@ class Solution {
     public int solution(int distance, int[] rocks, int n) {
         int answer = 0;
 
-        // 바위 정렬
         Arrays.sort(rocks);
 
-        // 이분 탐색 범위
         int left = 0;
         int right = distance;
 
-        // 최소구간을 찾아보자!
         while(left <= right) {
-            int mid = (left + right) / 2;
-            int brokenRocks = getBrokenRocks(rocks, mid, distance);
-            // 돌은 주어진 갯수보다 많이 부순 경우 최소구간 줄이기
-            if(brokenRocks > n) {
-                right = mid - 1;
-            // 주어진 수보다 적거나 같은 경우 -> 다시 탐색
-            } else {
+            int mid = (left + right)  / 2;
+            int bronkenRocks = getBrokenRocks(distance, rocks, mid);
+
+            if(bronkenRocks <= n) {
                 left = mid + 1;
-                answer = mid;   // 최대 값을 찾기 위해 계속 갱신
+                answer = mid;
+            } else {
+                right = mid - 1;
             }
         }
 
         return answer;
     }
 
-    // 최소 구간값(min)보다 작은 경우 돌을 부순다.
-    public int getBrokenRocks(int[] rocks, int min, int distance) {
+    public int getBrokenRocks(int distance, int[] rocks, int mid) {
         int cnt = 0;
-        int prev = 0;   // 출발지점부터 첫번째 돌까지의 거리
-        for(int rock : rocks) {
-            // 최소 구간값(min) 보다 적으면 돌을 부순다
-            if(rock - prev < min) {
+        int prev = 0;
+
+        for(int i=0; i<rocks.length; i++) {
+            if(rocks[i] - prev < mid) {
                 cnt++;
             } else {
-                prev = rock;
+                prev = rocks[i];
             }
         }
-
-        // 마지막돌부터 도착지점까지 거리 비교
-        if(distance - prev < min) cnt++;
+        if(distance - prev < mid) cnt++;
 
         return cnt;
     }
