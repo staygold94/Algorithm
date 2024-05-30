ea03ec429a36059814fbf40afc40d881b5038a1d
개발바닥 아방수 <98931608+staygold94@users.noreply.github.com>
2024-05-30 10:41:51 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 66.29 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 95.7 MB, 시간: 99.92 ms
+메모리: 96.1 MB, 시간: 66.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 1일 21:35:5
+2024년 05월 30일 10:41:49
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,23 +1,24 @@
 import java.util.*;
 
 class Solution {
-   public String solution(String[] participant, String[] completion) {
+    public String solution(String[] participant, String[] completion) {
         String answer = "";
 
-        Map<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
 
-        // 해시맵에 참가자 명단을 넣는다.
-        for(Integer i=0; i<participant.length; i++) {
-            map.put(participant[i], i);
+        for(String str : participant) {
+            map.put(str, map.getOrDefault(str, 0)  + 1);
         }
 
-        // 해시맵에서 참가자 명단을 제거한다.
-        for(Integer i=0; i<completion.length; i++) {
-            map.remove(completion[i]);
+        for(String str : completion) {
+            map.put(str, map.getOrDefault(str, 0)  - 1);
         }
 
-        for(String key: map.keySet()) {
-            answer = key;
+        for(String key : map.keySet()) {
+            if(map.get(key) != 0) {
+                answer = key;
+                break;
+            }
         }
 
         return answer;
