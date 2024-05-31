5cd47b7590fb65b039b3c2e8cad668e85a227ca1
개발바닥 아방수 <98931608+staygold94@users.noreply.github.com>
2024-05-31 10:23:49 +0900

    [level 3] Title: 등굣길, Time: 0.59 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
 
 ### 성능 요약
 
-메모리: 52 MB, 시간: 0.73 ms
+메모리: 53.2 MB, 시간: 0.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 19일 08:59:57
+2024년 05월 31일 10:23:47
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,32 +1,30 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
 
-        int[][] dp = new int[n][m];
+        int[][] arr = new int[m][n];
 
-        // 물 웅덩이 값 표식
+        // 초기화
+        arr[0][0] = 1;
+        
+        // 물웅덩이 -1로 초기화
         for(int i=0; i<puddles.length; i++) {
-            dp[puddles[i][1]-1][puddles[i][0]-1] = -1;
+            arr[puddles[i][0]-1][puddles[i][1]-1] = -1;
         }
 
-        // 시작점 초기화
-        dp[0][0] = 1;
-
-        for(int i=0; i<dp.length; i++) {
-            for(int j=0; j<dp[i].length; j++) {
-                
-                // 웅덩이라면 0으로 초기화 후 continue
-                if(dp[i][j] == -1) {
-                    dp[i][j] = 0;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                // 물웅덩이로 갈 수 있는 방법은 0개
+                if (arr[i][j] == -1) {
+                    arr[i][j] = 0;
                     continue;
                 }
 
-                // 이전값이 있다면 나머지 값을 더해준다.
-                if(i != 0) dp[i][j] += dp[i-1][j] % 1000000007;
-                if(j != 0) dp[i][j] += dp[i][j-1] % 1000000007;
-
+                // 위에서 오는 것과 오른쪽에서 오는 방법을 더한다
+                if(i != 0) arr[i][j] += arr[i-1][j] % 1000000007;
+                if(j != 0) arr[i][j] += arr[i][j-1] % 1000000007;
             }
         }
 
-        return dp[n-1][m-1] % 1000000007;
+        return arr[m-1][n-1] % 1000000007;
     }
 }
