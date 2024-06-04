fc2f72e49013a8c40b14fd2ebbc19116dda81280
개발바닥 아방수 <98931608+staygold94@users.noreply.github.com>
2024-06-05 08:47:25 +0900

    [level 2] Title: 올바른 괄호, Time: 16.81 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 53 MB, 시간: 21.37 ms
+메모리: 52.7 MB, 시간: 16.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 3일 8:34:26
+2024년 06월 05일 08:47:22
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,30 +2,16 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-
-        // 스택 생성
+        
         Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
 
-        // 들어온 변수가 홀수인 경우 무조건 false 반환
-        if(s.length() % 2 != 0) {
-            return false;
-        } else {
-            for(int i=0; i<s.length(); i++) {
-                // 효율성을 위해 split 하지 않고, charAt 메서드를 사용하여 Char형으로 접근
-                Character c = s.charAt(i);
-    
-                // 처음부터 닫는괄호가 나오면 false 리턴
-                if(stack.isEmpty() && c.equals(')')) {
-                    return false;
-    
-                // 여는 괄호면 스택에 쌓고, 닫는 괄호면 스택에서 여는 괄호를 하나 제거한다
-                } else {
-                    if(c.equals('(')) {
-                        stack.push(c);
-                    } else {
-                        stack.pop();
-                    }
-                }
+            if(ch == '(') {
+                stack.push(ch);
+            } else {
+                if(stack.isEmpty()) return false;
+                else stack.pop();
             }
         }
 
