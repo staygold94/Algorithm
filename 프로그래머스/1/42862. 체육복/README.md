f7de92f4b11d876b9012d6544ee94c4bcf494764
개발바닥 아방수 <98931608+staygold94@users.noreply.github.com>
2024-06-04 09:11:18 +0900

    [level 1] Title: 체육복, Time: 0.49 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 76.9 MB, 시간: 0.38 ms
+메모리: 77 MB, 시간: 0.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 29일 09:58:44
+2024년 06월 04일 09:11:16
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,15 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
+        // 전체수 - 도난당한수
+        int answer = n - lost.length;
 
-        // 들어온 배열 정렬 처리
         Arrays.sort(lost);
         Arrays.sort(reserve);
 
-        // 도난 당하지 않은 친구 수
-        int answer = n - lost.length;
-
-        // 도난 당했으나 여벌이 있는 친구 순 구해서 answer에 더함
+        // 도난당했으나 여별이 있는 친구
         for(int i=0; i<lost.length; i++) {
             for(int j=0; j<reserve.length; j++) {
                 if(lost[i] == reserve[j]) {
@@ -22,13 +20,14 @@ class Solution {
             }
         }
 
-        // 빌릴 수 있는 친구 카운트
+        // 빌릴 수 있는 친구
         for(int i=0; i<lost.length; i++) {
             for(int j=0; j<reserve.length; j++) {
-                if(lost[i] == reserve[j] - 1 || lost[i] == reserve[j] + 1) {
+                if(lost[i] == reserve[j] -1 || lost[i] == reserve[j] +1) {
                     lost[i] = -1;
                     reserve[j] = -1;
                     answer++;
+                    break;
                 }
             }
         }
