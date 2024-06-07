94ffef07c8721881f88be28329a799adbcd6c3ed
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-06-07 23:07:20 +0900

    [level 2] Title: 주식가격, Time: 21.67 ms, Memory: 67.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,25 +3,22 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        Stack<int[]> stack = new Stack<>();
-        for(int i=0; i<prices.length; i++) {
-            // stack에 값이 있다면
-            while (!stack.isEmpty()) {
-                int[] arr = stack.peek();
-                // 이전 주식가격이 더 높다면
-                if(prices[i] < arr[0]) {
-                    answer[arr[1]] = i - arr[1];
-                    stack.pop();
-                } else {
-                    break;
-                }
+        
+        // 중요 포인트! 인덱스만 저장해도 prices 값에 접근 가능하다는 점 (기존 풀이에는 인덱스까지 저장함)
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);  // 0번째 인덱스 push
+        for(int i=1; i<prices.length; i++) {
+            // stack에 값이 있다면 현재가가 더 높다면
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
             }
-            stack.push(new int[] {prices[i], i}); 
+            stack.push(i); 
         }
         
         while(!stack.isEmpty()) {
-            int[] arr = stack.pop();
-            answer[arr[1]] = prices.length - 1 - arr[1];
+            int idx = stack.pop();
+            answer[idx] = prices.length - 1 - idx;
         }
         
         return answer;
