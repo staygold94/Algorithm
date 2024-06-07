ec86b2b14cc646c8dc7579c6045a0fc3250d082c
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-06-07 22:58:15 +0900

    [level 2] Title: 주식가격, Time: 22.43 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,35 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        // 주식가격 길이만큼 정답배열 선언
-        int len = prices.length;
-        int[] answer = new int[len];
-
-        // 주식가격이 떨어지지 않고 유지되는 경우의 인덱스를 담을 스택
-        Stack<Integer> stack = new Stack<>();
-
-        // 주식가격 배열을 돌면서 인덱스 push
-        for(int i=0; i<len; i++) {
-
-            // 스택에 인덱스가 저장되어 있고,
-            // 최근 저장된 인덱스보다 가격이 떨어진 경우라면
-            // i - 최근인덱스값을 정답배열[최근인덱스값]에 넣어준다.
-            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-                answer[stack.peek()] = i - stack.peek();
-                stack.pop();
+        int[] answer = new int[prices.length];
+        Stack<int[]> stack = new Stack<>();
+        for(int i=0; i<prices.length; i++) {
+            // stack에 값이 있다면
+            while (!stack.isEmpty()) {
+                int[] arr = stack.peek();
+                // 이전 주식가격이 더 높다면
+                if(prices[i] < arr[0]) {
+                    answer[arr[1]] = i - arr[1];
+                    stack.pop();
+                } else {
+                    break;
+                }
             }
-            stack.push(i);
+            stack.push(new int[] {prices[i], i}); 
         }
-
-        // 주식가격 배열을 다 돌고 난 후
-        // 스택에 값이 없을 때까지 걸린 시간을 정답 배열에 넣어준다.
-        // 마지막 인덱스 기준으로 값을 넣어주어야 하는데, 이미 for문을 빠져나왔으므로
-        // 마지막 인덱스 값은 len - 1인 셈이다. 여기에 스택에 있던 인덱스 값을 빼주면 된다.
+        
         while(!stack.isEmpty()) {
-            answer[stack.peek()] = len - stack.peek() - 1;
-            stack.pop();
+            int[] arr = stack.pop();
+            answer[arr[1]] = prices.length - 1 - arr[1];
         }
-
+        
         return answer;
     }
 }
