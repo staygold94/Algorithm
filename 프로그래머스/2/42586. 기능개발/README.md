c156ec325ef2ef45acdc2a9120f7e87b23fd3111
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-06-08 18:08:06 +0900

    [level 2] Title: 기능개발, Time: 0.31 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 71.9 MB, 시간: 0.45 ms
+메모리: 74 MB, 시간: 0.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 1일 22:32:59
+2024년 06월 08일 18:10:09
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,6 +1,32 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
+        List<Integer> list = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        
+        // 총 걸리는 시간을 queue에 push
+        for(int i=0; i<progresses.length; i++) {
+            queue.offer((int)Math.ceil((100 - progresses[i]) / (double)speeds[i]));
+        }
+        
+        // queue에 값이 있다면 poll
+        while (!queue.isEmpty()) {
+            int num = queue.poll();
+            int cnt = 1;
+            // 선출 작업보다 빨리 끝난 작업이 있다면 같이 poll
+            while(!queue.isEmpty() && num >= queue.peek()) {
+                queue.poll();
+                cnt++;
+            }
+            list.add(cnt);
+        }   
+        
+        int[] answer = new int[list.size()];
+        for(int i=0 ;i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
         return answer;
     }
 }
