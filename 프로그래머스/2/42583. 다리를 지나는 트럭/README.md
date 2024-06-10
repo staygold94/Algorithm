b56864fc76a3541a1b0b8933824b7e9d55f8170b
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-06-10 16:01:36 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.17 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 76.4 MB, 시간: 0.15 ms
+메모리: 74.6 MB, 시간: 0.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 2일 21:51:7
+2024년 06월 10일 16:01:33
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,42 +2,35 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int second = 0;
-
+        int time = 0;
+        int total = 0;
+        int idx = 0;
         Queue<Integer> queue = new LinkedList<>();
 
-        // 다리 길이만큼 큐 기본값 0 채우기
+        // 다리를 0으로 가득 채움 (0은 트럭 없음)
         for(int i=0; i<bridge_length; i++) {
             queue.offer(0);
         }
-        
-        int sum = 0;    // 현재 무게
-        int idx = 0;    // 다리에 진입할 트럭 인덱스
 
-        // 마지막 트럭(인덱스)가 진입할 때까지 반복
+        // 트럭 리스트만큼 반복
         while(idx < truck_weights.length) {
-            // 다리길이 유지를 위해
-            // 큐에 이미 들어온 건 빼주고 시간은 더해준다.
-            while(queue.size() >= bridge_length) {
-                sum -= queue.poll();
-                second++;
-            }
+            // 가장 앞에 있는 것 빼기
+            int num = queue.poll();
+            total -= num;           // 토탈 무게 빼기
+            time++;                 // 시간 증가
 
-            // 들어올 트럭
-            int truck = truck_weights[idx];
-            // 총량을 넘지 않는다면 트럭을 넣고, 총량을 넘지 않으면 0을 넣어준다.
-            if(weight >= sum + truck) {
-                sum += truck;
-                queue.offer(truck);
-                idx++;
+            // 무게가 추가 되어도 되는 경우
+            if(weight >= total + truck_weights[idx]) {
+                total += truck_weights[idx];
+                queue.offer(truck_weights[idx]);    // 현재 인덱스의 트럭 추가
+                idx++;                              // 인덱스 이동
             } else {
                 queue.offer(0);
-            }  
+            }
         }
+        // 마지막으로 들어온 트럭에 대해서 다리길이만큼 시간 증가
+        time += bridge_length;
 
-        // 마지막 트럭이 나갈 수 있도록 다리 길이만큼 시간을 더해준다.
-        second += bridge_length;
-        
-        return second;
+        return time;
     }
 }
