970c6fcfb437475f798c8f096dec4e88615961a7
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-06-11 15:32:53 +0900

    [level 3] Title: 순위, Time: 12.34 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/README.md
+++ 프로그래머스/3/49191. 순위/README.md
@@ -1,10 +1,10 @@
 # [level 3] 순위 - 49191 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191) 
 
 ### 성능 요약
 
-메모리: 76.7 MB, 시간: 5.67 ms
+메모리: 87.5 MB, 시간: 12.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 02일 13:49:54
+2024년 06월 11일 15:32:52
 
 ### 문제 설명
 
--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,43 +1,32 @@
 class Solution {
     public int solution(int n, int[][] results) {
         int answer = 0;
-        int[][] match = new int[n][n];
+        int[][] arr = new int[n][n];
 
-        // 기본 세팅
-        for(int[] result : results){
-            int winner = result[0] - 1;
-            int loser = result[1] - 1;
-            match[winner][loser] = 1;     // win
-            match[loser][winner] = -1;    // lose
+        // 이긴 관계는 1, 진 관계는 -1로 표시 (관계를 알 수 없는 경우 기본값 0)
+        for(int[] result : results) {
+            arr[result[0]-1][result[1]-1] = 1;
+            arr[result[1]-1][result[0]-1] = -1;
         }
 
-        // a->b 이고 b->c이면 a->c 인 경우
-        for(int b=0; b<n; b++) {
-            for(int a=0; a<n; a++) {
-                // 진 경우는 제외
-                if(match[a][b] <= 0) {
-                    continue;
-                }
-
-                // 이긴 경우 (a->b)
-                for(int c=0; c<n; c++) {
-                    // b->c인 경우, 그리고 a와 c의 관계를 알 수 없는 경우
-                    if(match[b][c] > 0 && match[a][c] == 0) {
-                        match[a][c] = 1;
-                        match[c][a] = -1;
-                    }
+        // a -> b 이고 b -> c 이면, a -> c다
+        for(int k=0; k<arr.length; k++) {
+            for(int i=0; i<arr.length; i++) {
+                for(int j=0; j<arr.length; j++) {
+                    if(arr[i][k] == 1 && arr[k][j] == 1) arr[i][j] = 1;
+                    if(arr[i][k] == -1 && arr[k][j] == -1) arr[i][j] = -1;
                 }
             }
         }
 
-        for(int i=0; i<match.length; i++) {
-            int cnt = 0;
-            for(int j=0; j<match[i].length; j++) {
-                if(match[i][j] != 0) cnt++;
+        // 관계를 모두 알 수 있는 경우 answer++
+        for(int i=0; i<arr.length; i++) {
+            int count = 0;
+            for(int j=0; j<arr.length; j++) {
+                if(i == j || arr[i][j] != 0) count++;
             }
-            // n에서 자기 자신을 제외하고 (-1) 값이 있는 경우 순위를 알 수 있음
-            if(cnt == n - 1) answer++;
-        }   
+            if(count == n) answer++;
+        }
 
         return answer;
     }
