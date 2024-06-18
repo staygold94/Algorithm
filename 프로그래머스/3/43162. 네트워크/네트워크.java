ad0d5ce3b1f9ea209614ef044622d2c4d6f1f329
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-06-18 10:03:14 +0900

    [level 3] Title: 네트워크, Time: 0.22 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,15 +1,30 @@
+import java.util.*;
+
 class Solution {
     public int answer = 0;                          // 네트워크 갯수
     public boolean[] visited;                       // 방문여부
+    public HashMap<Integer, List<Integer>> map;     // 연결된 네트워크 해시맵
     public int solution(int n, int[][] computers) {
 
         // 방문여부 초기화
         visited = new boolean[n];
 
+        // computers 해시맵 생성
+        map = new HashMap<>();
+
+        // 해시맵에 연결된 네트워크 저장
+        for(int i=0; i<computers.length; i++) {
+            List<Integer> list = new ArrayList<>();
+            for(int j=0; j<computers[i].length; j++) {
+                if(computers[i][j] == 1 && i != j) list.add(j);
+            }
+            map.put(i, list);
+        }
+
         // dfs로 갯수 세기
         for(int i=0; i<computers.length; i++) {
             if(!visited[i]) {
-                dfs(i, computers);
+                dfs(i);
                 answer++;
             }
         }
@@ -17,15 +32,17 @@ class Solution {
         return answer;
     }
 
-    public void dfs(int idx, int[][] computers) {
+    public void dfs(int idx) {
 
         // 방문처리
         visited[idx] = true;
 
         // 더 방문할 곳이 있다면 방문
-        for(int i=0; i<computers[idx].length; i++) {
-            if(!visited[i] && computers[idx][i] == 1) {
-                dfs(i, computers);
+        List<Integer> list = map.get(idx);
+        for(int i=0; i<list.size(); i++) {
+            int nextIdx = list.get(i);
+            if(!visited[nextIdx]) {
+                dfs(nextIdx);
             }
         }
     }
