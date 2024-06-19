885cc35afd000ba2a7eba101ca3aa92c55ad10f2
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-06-19 09:04:17 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.70 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 56.7 MB, 시간: 7.14 ms
+메모리: 54.4 MB, 시간: 3.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 20일 16:02:51
+2024년 06월 19일 09:04:15
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,46 +1,57 @@
 import java.util.*;
 
 class Solution {
-    static int[][] distances; // 시작 정점으로부터 최단 거리를 저장할 배열
     public int solution(int[][] maps) {
 
-        // x축과 y축
-        int xSize = maps.length;
-        int ySize = maps[0].length;
+        // 가로 세로
+        int n = maps.length;
+        int m = maps[0].length;
 
-        // 최단거리 저장 배열 초기화
-        distances = new int[xSize][ySize];
+        // 거리 길이 재기
+        int[][] distances = new int[n][m];
+
+        // bfs를 위한 queue 준비
+        Queue<int[]> queue = new LinkedList<>();
+
+        // 시작점 넣기
+        queue.offer(new int[] {0, 0});
         distances[0][0] = 1;
 
-        // 큐 생성
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {0, 0});
+        // 동서남북 이동을 위한 배열
+        int[][] moves = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
         while(!queue.isEmpty()) {
-            int[] currentNode = queue.poll();
+            int[] now = queue.poll();
 
-            // 도착 노드인 경우 종료
-            if(currentNode[0] == xSize-1 && currentNode[1] == ySize-1) {
-                return distances[currentNode[0]][currentNode[1]];
+            // 도착지점인 경우 반환
+            if(now[0] == n-1 && now[1] == m-1) {
+                return distances[n-1][m-1];
             }
 
-            // 상하좌우 인접 노드를 탐색
-            int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-            for(int[] dir : directions) {
-                int nextX = currentNode[0] + dir[0];
-                int nextY = currentNode[1] + dir[1];
+            // 동서남북 이동
+            for(int i=0; i<moves.length; i++) {
+                int x = now[0] + moves[i][0];
+                int y = now[1] + moves[i][1];
 
-                // 그래프 범위 넘어갈시 continue 처리
-                if(nextX < 0 || nextX >= xSize || nextY < 0 || nextY >= ySize) continue;
+                // nxm 범위를 넘어가는 경우 pass
+                if(x < 0 || x >= n || y < 0 || y >= m) {
+                    continue;
+                }
 
-                // 아직 방문하지 않았고 벽이 아닌 경우 접근
-                if(distances[nextX][nextY] == 0 && maps[nextX][nextY] == 1) {
-                    queue.offer(new int[] {nextX, nextY});
-                    distances[nextX][nextY] = distances[currentNode[0]][currentNode[1]] + 1;
+                // 벽이 경우 pass
+                if(maps[x][y] == 0) {
+                    continue;
+                }
+
+                // 아직 접근하지 않았고, 길인 경우 (동서남북을 보므로 이미 할당한 거리에 대해서는 보지 않는다)
+                if(distances[x][y] == 0 && maps[x][y] == 1) {
+                    distances[x][y] = distances[now[0]][now[1]] + 1;
+                    queue.offer(new int[] {x, y});
                 }
             }
         }
 
+        // 접근 불가한 경우
         return -1;
     }
 }
