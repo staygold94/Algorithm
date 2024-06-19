be33320a61917c013de390ef212952f7ca42196a
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-06-19 11:15:24 +0900

    [level 3] Title: 단어 변환, Time: 0.03 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
 
 ### 성능 요약
 
-메모리: 77.4 MB, 시간: 0.03 ms
+메모리: 70.7 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 21일 10:16:56
+2024년 06월 19일 11:15:23
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,46 +1,48 @@
 class Solution {
     public boolean[] visited;
-    public int answer = 50;
+    public int answer;
     public int solution(String begin, String target, String[] words) {
 
-        // 초기화 (단어는 최대 50개이고 위치를 저장)
-        visited = new boolean[50];
-        
-        // 너비 탐색 시작
-        bfs(begin, target, words, 0);
-        
-        return answer == 50 ? 0 : answer;
+        // 초기화
+        answer = 0;
+        visited = new boolean[words.length];
+
+        // dfs
+        dfs(begin, target, words, 0);
+
+        return answer;
     }
 
-    public void bfs(String begin, String target, String[] words, int depth) {
-
-        // 깊이가 넘어가면 리턴
-        if(depth >= answer) return;
-
-        // target이 만들어지면
+    public void dfs(String begin, String target, String[] words, int depth) {
         if(begin.equals(target)) {
-            // 현재 depth와 answer 중에 작은 값을 answer에 넣는다.
-            answer = Math.min(answer, depth);
+            answer = depth;
+            return;
         }
-        
-        // 현재 단어 begin을 가지고 words를 순회하며 글자가 한개만 다른 걸 찾는다.
+
         for(int i=0; i<words.length; i++) {
-            // 방문하지 않았고, 글자가 한개만 다른 경우
+            // 방문하지 않았고, 1개의 차이가 난다면
             if(!visited[i] && isTarget(begin, words[i])) {
-                visited[i] = true;                      // 방문처리
-                bfs(words[i], target, words, depth+1);  // 재귀 순회
-                visited[i] = false;                     // 재귀 종료되어 다시 돌아오면 false 처리 해준다 !!
+                visited[i] = true;
+                dfs(words[i], target, words, depth+1);
+                visited[i] = false;
             }
         }
-    
-        return;
     }
 
     public boolean isTarget(String a, String b) {
+        
+        // 길이가 다른 경우 false 반환
+        if(a.length() != b.length()) {
+            return false;
+        }
+
+        // 한개의 문자만 다른지 확인
         int cnt = 0;
         for(int i=0; i<a.length(); i++) {
             if(a.charAt(i) != b.charAt(i)) cnt++;
+            if(cnt > 1) return false;
         }
-        return cnt == 1 ? true : false;
+
+        return true;
     }
 }
