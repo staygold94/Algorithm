8d721469d577d6dc1fdf83f10fe0f8f632d0edf1
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-06-20 09:36:18 +0900

    [level 1] Title: 체육복, Time: 0.45 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        // 전체수 - 도난당한수
+        // 도난 당하지 않은 인원
         int answer = n - lost.length;
-
+        
         Arrays.sort(lost);
         Arrays.sort(reserve);
 
-        // 도난당했으나 여별이 있는 친구
+        // 도난 당했으나 가져온 인원 구하기
         for(int i=0; i<lost.length; i++) {
             for(int j=0; j<reserve.length; j++) {
                 if(lost[i] == reserve[j]) {
@@ -20,10 +20,10 @@ class Solution {
             }
         }
 
-        // 빌릴 수 있는 친구
+        // 빌릴 수 인원 구하기
         for(int i=0; i<lost.length; i++) {
             for(int j=0; j<reserve.length; j++) {
-                if(lost[i] == reserve[j] -1 || lost[i] == reserve[j] +1) {
+                if(lost[i] == reserve[j] - 1 || lost[i] == reserve[j] + 1) {
                     lost[i] = -1;
                     reserve[j] = -1;
                     answer++;
