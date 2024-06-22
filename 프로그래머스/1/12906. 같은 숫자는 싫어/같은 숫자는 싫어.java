22e31b4e1a07ca72b63542678f5b00a05520cb7e
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-06-22 11:42:27 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 42.17 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,25 +1,20 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int []arr) {
-        // 스택 생성
-        Stack<Integer> stack = new Stack<>();
-        for(int i:arr) {
-            // 빈 스택이 아닌 경우
-            if(!stack.isEmpty()) {
-                if(stack.peek() != i) stack.push(i);  // 후입과 동일한 숫자가 아닌 경우 push
-            // 빈 스택인 경우
-            } else {
-                stack.push(i);
-            } 
+    public int[] solution(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<arr.length; i++) {
+            // 이전 숫자와 연속되는 경우 continue;
+            if(list.size() > 0) {
+                int num = list.get(list.size()-1);
+                if(arr[i] == num) {
+                    continue;
+                }
+            }
+            list.add(arr[i]);
         }
 
-        // 스택 길이만큼 answer 배열 선언
-        int[] answer = new int[stack.size()];
-        for(int i=answer.length-1; i >= 0; i--) {  // 배열 맨뒤부터 스택 pop 하여 저장
-            answer[i] = stack.pop();
-        }
-
-        return answer;
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
