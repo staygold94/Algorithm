69af05be702c5c3a4f5d0899f2dd17c708bcb951
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-06-22 11:05:49 +0900

    [level 2] Title: 전화번호 목록, Time: 176.52 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,22 +2,20 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;  // 기본값
 
-        Set<String> set = new HashSet<>();
-        for(String s : phone_book) {
-            set.add(s);
+        HashSet<String> set = new HashSet<>();
+        for(int i=0; i<phone_book.length; i++) {
+            set.add(phone_book[i]);
         }
-
-        // 자른 문자열이 다른 번호와 동일한지 확인 (접두어인지)
+        
         for(int i=0; i<phone_book.length; i++) {
             for(int j=1; j<phone_book[i].length(); j++) {
-                if(set.contains(phone_book[i].substring(0,j))) {
-                    answer = false;
-                    break;
+                if(set.contains(phone_book[i].substring(0, j))) {
+                    return false;
                 }
             }
         }
-        return answer;
+        
+        return true;
     }
 }
