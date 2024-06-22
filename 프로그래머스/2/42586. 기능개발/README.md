f90c52f099d93049d0e64535d7dc04564d492609
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-06-22 12:09:54 +0900

    [level 2] Title: 기능개발, Time: 1.98 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
 
 ### 성능 요약
 
-메모리: 74 MB, 시간: 0.31 ms
+메모리: 77.1 MB, 시간: 1.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 08일 18:10:09
+2024년 06월 22일 12:12:10
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -5,28 +5,30 @@ class Solution {
         List<Integer> list = new ArrayList<>();
         Queue<Integer> queue = new LinkedList<>();
         
-        // 총 걸리는 시간을 queue에 push
         for(int i=0; i<progresses.length; i++) {
-            queue.offer((int)Math.ceil((100 - progresses[i]) / (double)speeds[i]));
+            queue.offer(i);
         }
         
-        // queue에 값이 있다면 poll
-        while (!queue.isEmpty()) {
-            int num = queue.poll();
-            int cnt = 1;
-            // 선출 작업보다 빨리 끝난 작업이 있다면 같이 poll
-            while(!queue.isEmpty() && num >= queue.peek()) {
+        int day = 1;
+        int cnt = 0;
+        
+        while(!queue.isEmpty()) {
+            // 완료된 작업이 있다면 큐에서 제거
+            int idx = queue.peek();
+            if(progresses[idx] + speeds[idx] * day >= 100) {
                 queue.poll();
                 cnt++;
+            } else {
+                if(cnt != 0) {
+                    list.add(cnt);
+                    cnt = 0;
+                }
+                day++;
+                             
             }
-            list.add(cnt);
-        }   
-        
-        int[] answer = new int[list.size()];
-        for(int i=0 ;i<list.size(); i++) {
-            answer[i] = list.get(i);
         }
+        if(cnt != 0) list.add(cnt);
         
-        return answer;
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
