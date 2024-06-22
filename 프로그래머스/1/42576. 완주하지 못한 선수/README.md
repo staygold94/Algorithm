0fd1adcb247f4e0f4d8990b088b440cee265ed3f
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-06-22 10:34:58 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.65 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 96.1 MB, 시간: 66.29 ms
+메모리: 95.1 MB, 시간: 68.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 30일 10:41:49
+2024년 06월 22일 10:37:11
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,24 +3,28 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-
+        
         HashMap<String, Integer> map = new HashMap<>();
-
-        for(String str : participant) {
-            map.put(str, map.getOrDefault(str, 0)  + 1);
+        
+        // 참여자 명단 put
+        for(int i=0; i<participant.length; i++) {
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
         }
-
-        for(String str : completion) {
-            map.put(str, map.getOrDefault(str, 0)  - 1);
-        }
-
-        for(String key : map.keySet()) {
-            if(map.get(key) != 0) {
-                answer = key;
-                break;
+        
+        // 완주자 명단 update
+        for(int i=0; i<completion.length; i++) {
+            if(map.get(completion[i]) > 1) {
+                map.put(completion[i], map.getOrDefault(completion[i], 0) - 1);
+            } else {
+                map.remove(completion[i]);
             }
         }
-
+        
+        // 남아있는 key가 완주하지 못한 선수
+        for(String str : map.keySet()) {
+            answer = str;
+        } 
+        
         return answer;
     }
 }
