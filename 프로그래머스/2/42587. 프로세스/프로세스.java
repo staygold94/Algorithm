7d0c233b0479a93517d8609b8c208c7d689742e5
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-06-24 09:43:26 +0900

    [level 2] Title: 프로세스, Time: 0.67 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,29 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0; // 순서
+        int answer = 0;
+        Queue<Integer> queue = new LinkedList<>();
 
-        // 우선순위 큐를 사용하는 문제
-        // PriorityQueue 생성자에 Comparator 인터페이스를 구현한 Collections.reverseOrder()를 인수로 넣어준다.
-        // => 내림차순 우선순위 큐
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
-
-        // 들어온 대기 배열을 우선순위 큐에 넣는다
-        for(int i:priorities) {
+        for(int i=0; i<priorities.length; i++) {
             queue.offer(i);
         }
 
-        // 우선순위 큐에 값이 있는 동안 반복문 수행
+        int[] sortedArr = Arrays.copyOf(priorities, priorities.length);
+        Arrays.sort(sortedArr);             // 우선순위 정렬
+        int idx = sortedArr.length - 1;     // 현재 가장 높은 우선순위
+
         while(!queue.isEmpty()) {
-            for(int i=0; i<priorities.length; i++) {
-                // 우선순위 큐에서 가장 높은 값과 대기 배열의 값이 일치하는 경우
-                // 순서를 1 더해지고, 우선순위 큐에서 비교한 값을 제거한다
-                if(priorities[i] == queue.peek()) {
-                    answer++;
-                    queue.poll();
-                    // i번째와 알고 싶은 프로세스 위치가 일치하는 경우
-                    if(i == location) return answer;
+            int num = queue.peek();
+
+            // 우선순위가 가장 높다면
+            if(priorities[num] == sortedArr[idx]) {
+                answer++;
+                // 현재 프로세스가 특정 프로세스라면 정답이므로 break
+                if(num == location) {
+                    break;
                 }
+                queue.poll();
+                idx--;
+            } else {
+                queue.offer(queue.poll());
             }
         }
 
