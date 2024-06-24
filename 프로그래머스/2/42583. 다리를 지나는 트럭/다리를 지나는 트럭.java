5abbac4de514d9e084824121096e00c4761d0d0a
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-06-24 11:26:27 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.13 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,34 +2,46 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
+        
+        Queue<int[]> queue = new LinkedList<>();
         int time = 0;
-        int total = 0;
         int idx = 0;
-        Queue<Integer> queue = new LinkedList<>();
+        int total = 0;
 
-        // 다리를 0으로 가득 채움 (0은 트럭 없음)
-        for(int i=0; i<bridge_length; i++) {
-            queue.offer(0);
-        }
-
-        // 트럭 리스트만큼 반복
+        // 모든 트럭을 다리로 올리기
         while(idx < truck_weights.length) {
-            // 가장 앞에 있는 것 빼기
-            int num = queue.poll();
-            total -= num;           // 토탈 무게 빼기
-            time++;                 // 시간 증가
+            time++;
 
-            // 무게가 추가 되어도 되는 경우
-            if(weight >= total + truck_weights[idx]) {
-                total += truck_weights[idx];
-                queue.offer(truck_weights[idx]);    // 현재 인덱스의 트럭 추가
-                idx++;                              // 인덱스 이동
-            } else {
-                queue.offer(0);
+            // 다리를 다 건넌 트럭이 있는지 확인 -> 있는 경우 빠져나옴
+            if(!queue.isEmpty()) {
+                int[] arr = queue.peek();
+                if(time - arr[0] == bridge_length) {
+                    total -= arr[1];
+                    queue.poll();
+                }
+            }
+
+            // 다리가 비어있거나 무게를 초과하지 않는 경우
+            if(queue.isEmpty() || weight >= total + truck_weights[idx]) {
+
+                int[] arr = new int[2];
+                arr[0] = time;
+                arr[1] = truck_weights[idx];
+                total += arr[1];
+                queue.offer(arr);
+                idx++;
             }
         }
-        // 마지막으로 들어온 트럭에 대해서 다리길이만큼 시간 증가
-        time += bridge_length;
+
+        // 다리를 다 건널 때까지
+        while(!queue.isEmpty()) {
+            time++;
+
+            int[] arr = queue.peek();
+            if(time - arr[0] == bridge_length) {
+                queue.poll();
+            }
+        }
 
         return time;
     }
