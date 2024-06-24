63fc55fc65cad403395c3fd895bfb60afe9c0d03
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-06-24 15:58:03 +0900

    [level 2] Title: 주식가격, Time: 21.87 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=java) 
 
 ### 성능 요약
 
-메모리: 67.3 MB, 시간: 21.67 ms
+메모리: 63.7 MB, 시간: 21.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 07일 23:09:25
+2024년 06월 24일 15:58:00
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,24 +3,28 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        
-        // 중요 포인트! 인덱스만 저장해도 prices 값에 접근 가능하다는 점 (기존 풀이에는 인덱스까지 저장함)
         Stack<Integer> stack = new Stack<>();
-        stack.push(0);  // 0번째 인덱스 push
-        for(int i=1; i<prices.length; i++) {
-            // stack에 값이 있다면 현재가가 더 높다면
-            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-                int idx = stack.pop();
-                answer[idx] = i - idx;
+        int idx = 0;
+        int time = 0;
+
+        // 스택에 쌓기
+        while(idx < prices.length) {
+            // 주식가격이 내려가는 경우 pop
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[idx]) {
+                int popInx = stack.pop();
+                answer[popInx] = time - popInx;
             }
-            stack.push(i); 
+            stack.push(idx);
+            idx++;
+            time++;
         }
-        
+
+        // 스택값이 있는 경우 pop
         while(!stack.isEmpty()) {
-            int idx = stack.pop();
-            answer[idx] = prices.length - 1 - idx;
+            int popInx = stack.pop();
+            answer[popInx] = time - popInx - 1;
         }
-        
+
         return answer;
     }
 }
