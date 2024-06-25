42b03a61c6fafa19de91e40f78249131e3250e4e
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-06-26 08:12:15 +0900

    [level 2] Title: 가장 큰 수, Time: 1.64 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
 
 ### 성능 요약
 
-메모리: 78.7 MB, 시간: 1.76 ms
+메모리: 76.4 MB, 시간: 1.64 ms
 
 ### 구분
 
@@ -12,11 +12,11 @@
 
 ### 채점결과
 
-정확성: 100.0<br/>효율성: 0.0<br/>합계: 100.0 / 100.0
+정확성: 100.0<br/>합계: 100.0 / 100.0
 
 ### 제출 일자
 
-2024년 05월 18일 17:51:52
+2024년 06월 26일 08:12:14
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,36 +2,20 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] arr = new String[numbers.length];
-        StringBuilder sb = new StringBuilder();
 
+        String[] arr = new String[numbers.length];
         for(int i=0; i<numbers.length; i++) {
             arr[i] = String.valueOf(numbers[i]);
         }
 
-        // 음수를 반환하면 첫 번째 인자가 두 번째 인자보다 작음을 의미합니다.
-        // 0을 반환하면 두 인자가 서로 같음을 의미합니다.
-        // 양수를 반환하면 첫 번째 인자가 두 번째 인자보다 큼을 의미합니다.
+        Arrays.sort(arr, (s1, s2) -> (s2+s1).compareTo(s1+s2));
 
-        Arrays.sort(arr, (o1, o2) -> ((o2 + o1).compareTo(o1 + o2)));
-
-        // 9, 5 -> 59 95
-        // 음수 반환
-        // 첫번째 인자가 더 작다고 판단하여 정렬 앞으로 옴 
-
-        for(int i=0; i<numbers.length; i++) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<arr.length; i++) {
             sb.append(arr[i]);
+            if(i == 0 && arr[i].equals("0")) break;
         }
-        return (arr[0].equals("0") ? "0" : sb.toString());
-    }
-
-    public static void main(String[] args) {
-
-        Solution sol = new Solution();
-
-        int[] numbers = {3, 30, 34, 5, 9};
-        String answer = sol.solution(numbers);
-
-        System.out.println(answer);
+        
+        return sb.toString();
     }
 }
