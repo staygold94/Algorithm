040f8b6ea4fd019be24ff51f0c6c33e91f8faf74
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-06-27 10:25:20 +0900

    [level 2] Title: 소수 찾기, Time: 4.05 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
 
 ### 성능 요약
 
-메모리: 69.7 MB, 시간: 3.11 ms
+메모리: 72.8 MB, 시간: 4.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 07일 21:51:57
+2024년 06월 27일 10:25:18
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,39 +1,47 @@
 import java.util.*;
 
 class Solution {
-    public String[] nums;
-    public Set<Integer> primes;
+    HashSet<Integer> set;
     public int solution(String numbers) {
+        set = new HashSet<>();  // 소수
 
-        nums = numbers.split("");
-        primes = new HashSet<>();
+        // numbers 배열화
+        String[] arr = numbers.split("");
+        // 방문경로 초기화
+        boolean[] visited = new boolean[arr.length];
 
-        boolean[] visited = new boolean[nums.length];
-        dfs(visited, "");
-        
-        return primes.size();
+        dfs(arr, visited, 0, "");
+
+        return set.size();
     }
 
-    public void dfs(boolean[] visited, String num) {
+    public void dfs(String[] arr, boolean[] visited, int depth, String str) {
 
-        if(!num.equals("") && isPrime(Integer.parseInt(num))) {
-            primes.add(Integer.parseInt(num));
+        if(!str.equals("") && isPrime(Integer.parseInt(str))) {
+            set.add(Integer.parseInt(str));
+            System.out.println(str);
         }
 
-        for(int i=0; i<nums.length; i++) {
+        for(int i=0; i<arr.length; i++) {
             if(!visited[i]) {
                 visited[i] = true;
-                dfs(visited, num + nums[i]);
+                dfs(arr, visited, depth+1, str + arr[i]);
                 visited[i] = false;
             }
         }
     }
 
     public boolean isPrime(int num) {
-        if(num < 2) return false;
+        // 소수는 1과 자신으로 나눠져야 한다. 고로 2보다 작으면 false
+        if(num < 2) {
+            return false;
+        }
+
+        // 2부터 제곱근 전까지 나눠지는지 확인, 나눠지면 false
         for(int i=2; i<=Math.sqrt(num); i++) {
             if(num%i == 0) return false;
         }
+
         return true;
     }
 }
