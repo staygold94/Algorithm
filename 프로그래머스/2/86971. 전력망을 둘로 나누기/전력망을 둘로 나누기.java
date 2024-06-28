bfb298c6e0c27933a24730c2116f1684300446e8
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-06-28 10:38:17 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.29 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,65 +1,64 @@
 import java.util.*;
 
 class Solution {
-    static ArrayList<Integer>[] graph;  // 그래프를 표현할 ArrayList 배열
-    static int min;    // 두 전력망의 송전탑 개수 차이 중 최솟값을 저장할 변수
-
     public int solution(int n, int[][] wires) {
-        
-        graph = new ArrayList[n+1]; // 노드 개수만큼 ArrayList 생성
-        min = Integer.MAX_VALUE;    // 초기값을 최대값으로 설정하여 최솟값을 찾음
+        int answer = Integer.MAX_VALUE;
 
-        // 각 노드에 대한 ArrayList 초기화
-        for(int i=1; i<=n; i++) {
-            graph[i] = new ArrayList<>();
+        // 그래프 연결을 담을 graph
+        List<List<Integer>> graph = new ArrayList<>();
+
+        // 기본 전선 세팅
+        for(int i=0; i<=n; i++) {
+            graph.add(new ArrayList<>());
         }
 
-        // 간선 정보를 그래프에 추가
+        // 연결된 전선 list에 add
         for(int i=0; i<wires.length; i++) {
-            int v1 = wires[i][0];   // 간선의 한 끝점
-            int v2 = wires[i][1];   // 간선의 다른 끝점
-            // 양방향 간선이므로 두 번 추가
-            graph[v1].add(v2);
-            graph[v2].add(v1);
+            graph.get(wires[i][0]).add(wires[i][1]);
+            graph.get(wires[i][1]).add(wires[i][0]);
         }
 
-        // 각 간선을 하나씩 제거하면서 송전탑 개수 차이 계산
-        for(int i=0; i<wires.length; i++) {
-            int v1 = wires[i][0];   // 간선의 한 끝점
-            int v2 = wires[i][1];   // 간선의 다른 끝점
+        // 하나씩 제거해보며 차이가 가장 적은 네트워크 수를 구해보자
+        for(int[] wire : wires) {
+            int node1 = wire[0];
+            int node2 = wire[1];
 
-            boolean[] visited = new boolean[n+1];   // 방문 여부를 저장할 배열 초기화
+            // 간선 제거
+            graph.get(node1).remove((Integer)node2);
+            graph.get(node2).remove((Integer)node1);
 
-            // 해당 간선을 그래프에서 제거
-            graph[v1].remove(Integer.valueOf(v2));
-            graph[v2].remove(Integer.valueOf(v1));
+            int netCnt = bfs(graph, n, node1);
+            int anotherCnt = n - netCnt;
 
-            // 임의의 시작점에서 DFS 탐색하여 송전탑 개수 계산
-            int cnt = dfs(1, visited);
-            
-            // 두 전력망의 송전탑 개수 차이 계산
-            int diff = Math.abs(cnt - (n - cnt));
-            // 최솟값 갱신
-            min = Math.min(min, diff);
+            answer = Math.min(answer, Math.abs(netCnt - anotherCnt));
 
-            // 제거한 간선을 다시 그래프에 추가하여 다음 간선 제거를 위해 복구
-            graph[v1].add(v2);
-            graph[v2].add(v1);
-
+            // 간선 복구
+            graph.get(node1).add(node2);
+            graph.get(node2).add(node1);
         }
 
-        return min; // 최솟값 반환
+        return answer;
     }
 
-    // DFS를 통해 송전탑 개수 계산
-    static int dfs(int v, boolean[] visited) {
-        visited[v] = true;  // 현재 노드 방문 처리
-        int cnt = 1;    // 현재 노드 포함하여 송전탑 개수 1개로 초기화
+    public int bfs(List<List<Integer>> graph, int n, int startIdx) {
 
-        // 현재 노드와 인접한 노드들에 대해 DFS 재귀 호출
-        for(int next:graph[v]) {
-            if(!visited[next]) {
-                cnt += dfs(next, visited);
+        int cnt = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[n+1];
+
+        queue.offer(startIdx);
+        visited[startIdx] = true;
+        
+        while(!queue.isEmpty()) {
+            // 방문처리하고 네트워크수 추가
+            int num = queue.poll();
+            cnt++;
+            
+            for(int node : graph.get(num)) {
+                if(!visited[node]) {
+                    queue.offer(node);
+                    visited[node] = true;
+                }
             }
         }
 
