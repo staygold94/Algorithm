1f74344f109974cc1650cd69fd5d8a004f5ed1e9
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-07-01 13:43:18 +0900

    [level 3] Title: 정수 삼각형, Time: 6.11 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=java) 
 
 ### 성능 요약
 
-메모리: 60 MB, 시간: 5.83 ms
+메모리: 58.2 MB, 시간: 6.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 30일 12:57:00
+2024년 07월 01일 13:43:17
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int[][] triangle) {
 
-        for(int i=triangle.length - 1; i > 0; i--) {
-            for(int j=0; j<i; j++) {
+        for(int i=triangle.length - 1; i>0; i--) {
+            for(int j=0; j<triangle[i].length-1; j++) {
                 triangle[i-1][j] += Math.max(triangle[i][j], triangle[i][j+1]);
             }
         }
