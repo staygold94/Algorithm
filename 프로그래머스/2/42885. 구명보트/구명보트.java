96019b3e118435f815cbe99d0ec0397970db9f12
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-07-01 13:22:28 +0900

    [level 2] Title: 구명보트, Time: 11.40 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,28 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0; // 보트 나르는 횟수
-        int idx = 0; // 실어야 하는 사람 인덱스
+        int answer = 0;
 
-        // 몸무게 오름차순 정렬
         Arrays.sort(people);
-        int sum = 0;
 
-        // 가벼운 사람부터 마지막 사람까지 반복문을 돈다.
-        while(idx < people.length) {
-            // 배에 탈 수 있다면
-            if(people[idx] <= limit - sum) {
-                sum += people[idx]; // 현재 무게 추가
-                idx++;              // 다음 사람 인덱스
+        int left = 0;
+        int right = people.length - 1;
 
-            // 배에 탈 수 없다면
-            } else {
-                answer++;   // 보트 나르는 횟수 증감
-                sum = 0;    // 현재 무게 초기화
+        
+        while(left <= right) {
+            int total = 0;
+            total += people[right];
+            right--;
+            
+            if(left <= right && total + people[left] <= limit) {
+                left++;
             }
+            answer++;
         }
 
-        // 배에 현재 실린 사람이 있다면 증감하여 리턴
-        return sum == 0 ? answer : ++answer;
+        return answer;
     }
 }
