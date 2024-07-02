3865f605b0f79695161a08f761238354438d5f9f
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-07-02 10:36:20 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18296 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,37 +1,27 @@
 import java.io.*;
 
 public class Main {
-
-    static Integer[] dp;
-
     public static void main(String[] args) throws IOException {
-
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        dp = new Integer[n+1];  // 각 인덱스 별로 숫자당 몇번이 필요한지 저장
-        dp[0] = dp[1] = 0;
 
-        for(int i=2; i<=n; i++) {
-            recur(i);
-        }
+        int[] dp = new int[n+1];
+        if(n >= 1) dp[1] = 0;
+        if(n >= 2) dp[2] = 1;
+        if(n >= 3) dp[3] = 1;
 
-        System.out.println(recur(n));
-    }
-
-    // Top-down 방식의 재귀호출
-    static int recur(int n) {
-        if(dp[n] == null) {
-            if(n % 6 == 0) {
-                dp[n] = Math.min(recur(n-1), Math.min(recur(n/3), recur(n/2))) + 1;
-            } else if(n % 3 == 0) {
-                dp[n] = Math.min(recur(n-1), recur(n/3)) + 1;
-            } else if(n % 2 == 0) {
-                dp[n] = Math.min(recur(n-1), recur(n/2)) + 1;
+        for(int i=4; i<=n; i++) {
+            if(i % 6 == 0) {
+                dp[i] = Math.min(dp[i-1] + 1, Math.min(dp[i/2] + 1, dp[i/3] + 1));
+            } else if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i/2] + 1, dp[i-1] + 1);
+            } else if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i/3] + 1, dp[i-1] + 1);
             } else {
-                dp[n] = recur(n-1) + 1;
+                dp[i] = dp[i-1] + 1;
             }
         }
-        return dp[n];
+        System.out.println(dp[n]);
     }
 }
