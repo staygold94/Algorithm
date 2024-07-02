e9c62be1b837b0397e33e2e62af0ece7df9a4b25
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-07-02 22:56:52 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,58 +1,31 @@
 import java.io.*;
 
 public class Main {
-
-    static Long[][] dp; // 자릿수가 100까지 될 수 있으므로 Long으로 선언
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        dp = new Long[n+1][10];
+        // 0~9까지 총 10개의 숫자 + n-1에 대비하여 양쪽으로 한 칸씩 확보
+        long[][] dp = new long[n][12];
 
-        // 첫째자리 초기화
-        for(int i=0; i<10; i++) {
-            if(i==0) {
-                dp[0][i] = 0L;
-            } else {
-                dp[0][i] = 1L;
+        // 초기값 설정 (1자리인 경우 1~9씩 각 1개를 가짐)
+        for(int i=2; i<=10; i++) {
+            dp[0][i] = 1L;
+        }
+
+        long mod = 1000000000L;
+        for(int i=1; i<n; i++) {
+            for(int j=1; j<=10; j++) {
+                // 값이 n인 개수 = 이전 행의 m-1 + 이전 행의 m+1 개수
+                dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % mod;
             }
         }
 
-        for(int i=1; i<n; i++){
-            for(int j=0; j<10; j++) {
-                solve(i,j);
-            }
+        long sum = 0L;
+        // 맨 마지막 n번째 때 0~9 숫자들의 갯수 더함
+        for(int i=1; i<=10; i++) {
+            sum += dp[n-1][i];
         }
-
-        // 2차원 배열 프린트 (n이 높아질수록 값이 달라지는지 확인하기)
-        // for(int i=0; i<n; i++) {
-        //     for(int j=0; j<10; j++) {
-        //         System.out.print(dp[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-
-        // 정답도 Long으로 선언
-        Long answer = 0L;
-
-        // n 자리의 계단수 합
-        for(int i=0; i<10; i++) {
-            answer += dp[n-1][i];  // answer에도 나누기 해주기
-        }
-
-        System.out.println(answer % 1000000000);
-
-    }
-
-    public static Long solve(int i, int j) {
-        if(j==0) {
-            dp[i][j] = dp[i-1][1] % 1000000000;
-        } else if(j==9) {
-            dp[i][j] = dp[i-1][8] % 1000000000;
-        } else {
-            dp[i][j] = ((dp[i-1][j-1] + dp[i-1][j+1]) % 1000000000);    // 괄호 실수 주의하기
-        }
-        return dp[i][j];
+        System.out.println(sum%mod);
     }
 }
