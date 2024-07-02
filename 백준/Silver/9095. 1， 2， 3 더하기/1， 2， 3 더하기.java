44decd9cbf9aa7c7a8eb94e1f2e93d5298ff16fc
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-07-02 21:03:35 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,35 +1,24 @@
 import java.io.*;
 
 public class Main {
-    static Integer[] dp;
-    
     public static void main(String[] args) throws IOException {
 
-        dp = new Integer[11];
-        dp[0] = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] dp = new int[12];
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 4;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int su = Integer.parseInt(br.readLine());   // 테스트 케이스
+        for(int i=4; i<=11; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
 
-        // 테스트 케이스만큼 반복
-        for(int j=0; j<su; j++) {
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<T; i++) {
             int n = Integer.parseInt(br.readLine());
-
-            for(int i=4; i<=n; i++) {
-                recur(i);
-            }
-            System.out.println(recur(n));
-        }
-    }
-
-    // 점화식을 알아차리는 게 dp 문제에서 핵심이지 않을까..
-    public static int recur(int n) {
-        if(dp[n] == null) {
-            dp[n] = dp[n-3] + dp[n-2] + dp[n-1];
-        }
-        return dp[n];
+            System.out.println(dp[n]);
+        }      
     }
 }
