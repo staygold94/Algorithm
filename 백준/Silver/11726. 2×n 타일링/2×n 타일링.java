22fae2ee883b5fa0e55de996b804f0da5e935f62
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-07-02 11:13:59 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,32 +1,18 @@
 import java.io.*;
 
 public class Main {
-
-    static Integer[] dp;
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        dp = new Integer[1001];
-        dp[0] = 0;
+        
+        int mod = 10007;
+        int[] dp = new int[n+1];
         dp[1] = 1;
-        dp[2] = 2;
+        if(n >= 2) dp[2] = 2;
 
-        // 재귀의 depth가 깊어지는 걸 막기 위해 미리 실행
         for(int i=3; i<=n; i++) {
-            recur(i);
+            dp[i] = (dp[i-1] + dp[i-2]) % mod;
         }
-
-        System.out.println(recur(n));
-    }
-
-    // 재귀함수 (top-down 방식)
-    public static int recur(int n) {
-        if(dp[n] == null && n>=3) {
-            dp[n] = (dp[n-2] + dp[n-1]) % 10007;
-        }
-        return dp[n];
+        System.out.println(dp[n]);
     }
 }
--- 백준/Silver/11726. 2×n 타일링/README.md
+++ 백준/Silver/11726. 2×n 타일링/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 14316 KB, 시간: 128 ms
+메모리: 14344 KB, 시간: 108 ms
 
 ### 분류
 
@@ -12,7 +12,7 @@
 
 ### 제출 일자
 
-2023년 10월 17일 10:00:04
+2024년 7월 2일 11:12:16
 
 ### 문제 설명
 
@@ -20,7 +20,7 @@
 
 <p>아래 그림은 2×5 크기의 직사각형을 채운 한 가지 방법의 예이다.</p>
 
-<p style="text-align: center;"><img alt="" src="" style="height:50px; width:125px"></p>
+<p style="text-align: center;"><img alt="" src="https://onlinejudgeimages.s3-ap-northeast-1.amazonaws.com/problem/11726/1.png" style="height:50px; width:125px"></p>
 
 ### 입력 
 
