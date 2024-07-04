372871bb88d4b633074f7683ab07b1343c7dba9d
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-07-04 15:55:39 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 156 ms, Memory: 14784 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -1,72 +1,30 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    static int n;
-    static int[] arr;
-    static Integer[] dp;
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-        dp = new Integer[n];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for(int i=0; i<n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        for(int i=0; i<n; i++) {
-            solve(i);
-        }
-
-        // dp의 값중 가장 큰 값을 찾아 출력
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] dp = new int[n];
         int answer = 0;
 
+        // 초기화
         for(int i=0; i<n; i++) {
-            answer = Math.max(answer, dp[i]);
-            // System.out.print(dp[i] + " ");
+            dp[i] = arr[i];
         }
 
-        System.out.println(answer);
-    }
-
-    static Integer solve(int n) {
-
-        // 빈값에 대해서만 재귀 호출
-        if(dp[n] == null) {
-
-            dp[n] = arr[n];  // 초기화
-
-            for(int i=n-1; i>=0; i--) {
-                if(arr[i] < arr[n]) {
-                    dp[n] = Math.max(dp[n], solve(i) + arr[n]);
+        // 증가값이라면 더하기
+        for(int i=0; i<n; i++) {
+            int max = 0;
+            for(int j=0; j<i; j++) {
+                if(arr[j] < arr[i]) { 
+                    max = Math.max(max, dp[j]);
                 }
             }
+            dp[i] += max;
+            if(answer < dp[i]) answer = dp[i];
         }
-
-        return dp[n];
+        System.out.println(answer);
     }
 }
-
-
-/*
-
-10
-1 100 2 50 60 3 5 6 7 8
-answer 113 (1 + 2 + 50 + 60 )
-
-5
-100 1 3 5 7
-answer 100
-
-6
-100 200 110 320 190 210
-answer 620
-
- */
