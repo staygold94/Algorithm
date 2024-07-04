84d89ed2029febbaec351aa4a5650b66eab68eab
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-07-04 11:40:32 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 14800 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -2,81 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    static int n;
-    static int[] arr;
-    static Integer[] dp;
-
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        int n = Integer.parseInt(br.readLine());
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-        dp = new Integer[n];
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] dp = new int[n];
 
         for(int i=0; i<n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
         }
 
         for(int i=0; i<n; i++) {
-            solve(i);
-        }
-
-        int max = dp[0];
-		// 최댓값 찾기 
-		for(int i = 1; i < n; i++) {
-			max = Math.max(max, dp[i]);
-		}
-		System.out.println(max);
-        
-    }
-
-    static int solve(int n) {
-
-        // # 탐색하지 않은 경우에만 for문을 타도록!!
-        if(dp[n] == null) {
-            dp[n] = 1;  // 초기화
-        
-
-            for(int i=n-1; i>=0; i--) {
-                if(arr[i] < arr[n]) {
-                    // # 재귀를 활용하는 게 포인트
-                    dp[n] = Math.max(dp[n], solve(i) + 1);
+            for(int j=0; j<i; j++) {
+                if(arr[j] < arr[i]){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
                 }
             }
         }
-        
-        return dp[n];
+
+        int answer = 0;
+        for(int i=0; i<n; i++) {
+            answer = Math.max(answer, dp[i]);
+        }
+
+        System.out.println(answer);
     }
-    
 }
-
-
-/*
-
-각 dp[] 의 길이들은 다음 부분 수열을 의미하는 것이다.
-# 이와 같이 dp에 무엇을 넣을지가 중요했다. 
-
-dp[0] = {10} : 길이 1
-dp[1] = {10, 20} : 길이 2
-dp[2] = {10} : 길이 1
-dp[3] = {10, 20, 30} : 길이 3
-dp[4] = {10, 20} : 길이 2
-dp[5] = {10, 20, 30, 50} : 길이 4
-
-6
-10 20 10 30 20 50
-answer 4
-
-7
-70 30 50 60 70 0 10
-answer 4
-
-8
-10 50 60 70 20 30 40 50
-answer 5
-
- */
