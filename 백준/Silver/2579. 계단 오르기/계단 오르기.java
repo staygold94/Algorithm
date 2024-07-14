49e444edfda7fe5cab2715ee54bef4705c084261
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-07-15 08:22:41 +0900

    [Silver III] Title: 계단 오르기, Time: 108 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,66 +1,26 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    
-    static int n;
-    static int[] arr;
-    static Integer[] dp;
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n+1];
+        int[] dp = new int[n+1];
 
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n+1];
-        dp = new Integer[n+1];
-
-        StringTokenizer st;
+        String[] test = {"10", "20", "15", "25", "10", "20"};
 
         for(int i=1; i<=n; i++) {
-
-            st = new StringTokenizer(br.readLine());
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        // 초기화
-        dp[0] = 0;
-        dp[1] = arr[1];
-
-        if(n>=2) {
-            dp[2] = arr[1] + arr[2];    
-        }
-        
+        // dp 초기값 설정
+        dp[0] = arr[0];
+        if(n>=1) dp[1] = arr[1];
+        if(n>=2) dp[2] = arr[1] + arr[2];
 
         for(int i=3; i<=n; i++) {
-            solve(i);
+            dp[i] = Math.max(arr[i] + arr[i-1] + dp[i-3], arr[i] + dp[i-2]);
         }
-
-        // 무조건 마지막 계단을 밟아야 하므로, max 값을 구하는 게 아니다! 함정 주의!
         System.out.println(dp[n]);
     }
-
-    static Integer solve(int n) {
-
-        if(dp[n] == null) {
-
-            dp[n] = Math.max(arr[n-1] + dp[n-3], dp[n-2]) + arr[n]; 
-        }
-
-        return dp[n];
-    }
 }
-
-
-/*
-
-6
-10
-20
-15
-25
-10
-20
-
-75
- */
