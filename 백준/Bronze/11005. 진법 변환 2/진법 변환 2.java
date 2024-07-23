e111bc08ce420fcb680e1e22c79caae6d24dbf25
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-07-23 21:55:15 +0900

    [Bronze I] Title: 진법 변환 2, Time: 128 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int num = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        String[] arr = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+        StringBuilder sb = new StringBuilder();
+
+        while(num > 0) {
+            sb.append(arr[num%b]);
+            num /= b;
+        }
+
+        System.out.println(sb.reverse());
+
+    }
+}
