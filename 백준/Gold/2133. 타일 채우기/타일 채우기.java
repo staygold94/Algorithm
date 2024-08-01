ddc8f0c580cf8c10b133cd02ff09efaea6048c53
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-08-01 19:59:51 +0900

    [Gold IV] Title: 타일 채우기, Time: 128 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.java
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -1,26 +1,29 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-
+        int n = Integer.parseInt(br.readLine());    // (1 ≤ N ≤ 30)
         int[] dp = new int[n+1];
 
-        dp[0] = 1;
+        // 홀수인 경우 0개
+        if(n % 2 == 1) {
+            dp[n] = 0;
 
-        for(int i=2; i<n+1; i++) {
-            // 1) 이전에 만들어진 짝수번째 타일의 개수 X 3
-            dp[i] = dp[i-2] * 3;
-            for(int j=i-4; j>=0; j-=2) {
-                // 2) 그 전전의 전체 타일 개수 X 이전타일의 특별한 타일 개수
-                // 3) 해당 위치에서 만들어지는 특별한 타일 개수 (j가 0일 때)
-                dp[i] += dp[j] * 2;
+        // 짝수인 경우
+        } else {
+            // 초기값
+            dp[0] = 1;
+            dp[2] = 3;
+
+            for(int i=4; i<=n; i+=2) {
+                dp[i] = dp[i-2] * dp[2];
+                for(int j=i-4; j>=0; j-=2) {
+                    dp[i] += dp[j] * 2;
+                }
             }
         }
-
         System.out.println(dp[n]);
     }
 }
