c7265ad9caca385b770f6678cb361061dd3e2903
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-08-01 09:46:30 +0900

    [Silver II] Title: 제곱수의 합, Time: 176 ms, Memory: 15368 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -1,55 +1,27 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+        int INF = Integer.MAX_VALUE;
 
-        // dp 배열 생성
-        Integer[] dp = new Integer[n+1];
+        Arrays.fill(dp, INF);
 
-        // 초기화 값 설정
+        // dp[0] 초기화
         dp[0] = 0;
 
-        // 재귀의 깊이가 쌓이지 않기 위해서 낮은 수부터 반복문
         for(int i=1; i<=n; i++) {
-            // 1^ + 1 ^ ... + 1^ 처럼 최대개수를 기본값으로 지정
-            dp[i] = i; 
+            // 기본 갯수 (1+1+1... 이런 식)
+            dp[i] = i;
 
-            // 제곱근까지 반복문 (=> 최소개수 조합을 찾기 위함)
-            for(int j=2; j*j <= i; j++) {
-                // 기본값과 다른 제곱근의 조합 중 최소개수를 dp의 값으로 지정 
-                dp[i] = Math.min(dp[i], 1 + dp[i-j*j]);
+            // 최소 갯수 찾기
+            for(int j=2; j*j<=i; j++) {
+                dp[i] = Math.min(dp[i], dp[i-j*j] + 1);
             }
         }
-
         System.out.println(dp[n]);
-    } 
+    }
 }
-
-
-/*
-
-11 3
-
-82009 : 2
-
-89005 : 3
-
-61495 : 4
-
-2542 : 3
-
-77882 : 3
-
-10198 : 3
-
-37961 : 3
-
-98952 : 3
-
-35057 : 3
-
- */
