8e7f48c3e500f507d0609c3a04395e5b39f55dc7
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-08-07 08:48:53 +0900

    [Bronze I] Title: 소인수분해, Time: 116 ms, Memory: 15900 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -4,26 +4,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        while(n > 1) {
-            for(int i=2; i<=n; i++) {
-                if(n % i == 0 && isPrime(i)) {
-                    System.out.println(i);
-                    n /= i;
-                    break;
-                }
+        for(int i=2; i<=Math.sqrt(n); i++) {
+            while(n % i == 0) {
+                n /= i;
+                sb.append(i + "\n");
             }
         }
-    }
 
-    public static boolean isPrime(int num) {
-
-        if(num == 1) return false;
-
-        for(int i=2; i<=Math.sqrt(num); i++) {
-            if(num % i == 0) return false;
-        }
-
-        return true;
+        if(n != 1) sb.append(n);
+        System.out.print(sb.toString());
     }
 }
