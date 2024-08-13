d4aec62da860579c0e8a34b9dbc8741f2433d495
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-08-13 10:11:59 +0900

    [Silver III] Title: 파도반 수열, Time: 124 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,40 +1,29 @@
 import java.io.*;
 
 public class Main {
-
-    static Long[] dp;
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        // 테스트 (1 ≤ N ≤ 100)
-        Long num = Long.parseLong(br.readLine());
-
-        // 케이스 갯수만큼 반복문
-        for(int i=0; i<num; i++) {
+        int t = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<t; i++) {
             int n = Integer.parseInt(br.readLine());
-            dp = new Long[n+1];
-            dp[0] = 0L;  // 초기값
-
-            for(int j=1; j<=n; j++) {
-                solve(j);
-            }
-            System.out.println(dp[n]);
+            sb.append(findDp(n) + "\n");
         }
+        System.out.print(sb.toString());
     }
 
-    static Long solve(int n) {
+    public static Long findDp(int n) {
+        long[] dp = new long[Math.max(n+1, 6)];
+        // 초기값 넣기
+        dp[1] = 1L;
+        dp[2] = 1L;
+        dp[3] = 1L;
+        dp[4] = 2L;
+        dp[5] = 2L;
 
-        if(dp[n] == null) {
-
-            if(n>4) {
-                dp[n] = dp[n-5] + dp[n-1];
-            } else if(n==4) {
-                dp[n] = 2L;
-            } else{
-                dp[n] = 1L;
-            }
+        for(int i=6; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-5];
         }
 
         return dp[n];
