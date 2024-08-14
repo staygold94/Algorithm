2312eabe1ea8ba23dc30493e2a276749741e50ef
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-08-14 10:01:08 +0900

    [Silver IV] Title: 카드, Time: 376 ms, Memory: 36052 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -1,49 +1,26 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-
-        // 숫자 카드의 범위가 -2e62~2e62 이므로 long 타입이 필요하고,
-        // 배열의 경우 너무 많은 메모리가 필요하기 때문에 HashMap으로 풀어본다.
-        HashMap<Long, Integer> map = new HashMap<Long, Integer>();
+        HashMap<Long, Integer> map = new HashMap<>();
 
         for(int i=0; i<n; i++) {
             long num = Long.parseLong(br.readLine());
-
-            // 이미 저장된 카드값이라면
-            if(map.containsKey(num)) {
-                map.put(num, map.get(num) + 1);
-            } else { // 처음 저장한다면
-                map.put(num, 1);
-            }
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        
-        br.close();
-        
-        int max_cnt = 0;    // 가장 많이 가지고 있는 수
-        long low_num = 0;   // 가장 많이 가지고 있는 수 중 가장 작은 수
 
-        // 저장된 모든 키를 얻는 keySet() 메서드
+        long answer = 0;
+        int max = 0;
         for(long key : map.keySet()) {
-            if (map.get(key) == max_cnt) {
-                if (key < low_num)
-                low_num = key;
-            } else if (map.get(key) > max_cnt) {
-                max_cnt = map.get(key);
-                low_num = key;
+            int num = map.get(key);
+            if(max < num || (max == num && answer > key)) {
+                answer = key;
+                max = num;
             }
         }
-        System.out.println(low_num);
-        
+        System.out.println(answer);
     }
 }
-
-/*
- * 참고하면 좋은 사이트
- * https://janeljs.github.io/algorithms/bj-11652-card/
- */
