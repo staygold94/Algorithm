2b8d3064dcd253e47a867b68329297bc3c851500
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-08-20 17:13:48 +0900

    [Bronze V] Title: A+B, Time: 100 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,20 +1,12 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int answer = 0;
-
-        // 토큰이 있는지 확인
-        while(st.hasMoreTokens()) {
-            answer += Integer.parseInt(st.nextToken());
-        } 
-
-        System.out.println(answer);
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(a+b);
     }
 }
