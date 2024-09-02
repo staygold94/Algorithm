914ed05ae8ebfa4e18e39b506664d491a6f0691a
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-09-03 08:53:25 +0900

    [Silver IV] Title: 국영수, Time: 704 ms, Memory: 65036 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.java
+++ 백준/Silver/10825. 국영수/국영수.java
@@ -1,66 +1,59 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static class person {
+        int kor, math, eng;
+        String name;
+
+        public person(String name, int kor, int math, int eng) {
+            this.name = name;
+            this.kor = kor;
+            this.math = math;
+            this.eng = eng;
+        }
+    }
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine()); 
-        Student[] arr = new Student[n];
-        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        person[] arr = new person[n];
 
         for(int i=0; i<n; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             String name = st.nextToken();
             int kor = Integer.parseInt(st.nextToken());
             int eng = Integer.parseInt(st.nextToken());
-            int mat = Integer.parseInt(st.nextToken());
-            arr[i] = new Student(name, kor, eng, mat);
+            int math = Integer.parseInt(st.nextToken());
+            person p = new person(name, kor, math, eng);
+            arr[i] = p;
         }
 
-        // !! Arrays.sort는 비교되는 반환값이 음수면 자리를 바꾼다. !!
-        // !! 람다식으로 비교 및 정렬 기준의 파라미터를 넘길 수 있다. !!
-        Arrays.sort(arr, new Comparator<Student> () {
+        Arrays.sort(arr, new Comparator<person>() {
 
             @Override
-            public int compare(Student o1, Student o2) {
-
-                if (o1.kor == o2.kor) {
-                    if (o1.eng == o2.eng) {
-                        if (o1.mat == o2.mat) {
-                            // !! String 비교는 compareTo 메서드로 가능하다 !!
-                            return o1.name.compareTo(o2.name);
+            public int compare(person p1, person p2) {
+                if(p1.kor == p2.kor) {
+                    if(p1.eng == p2.eng) {
+                        if(p1.math == p2.math) {
+                            return p1.name.compareTo(p2.name);
+                        } else{
+                            return p2.math - p1.math;
                         }
-                        return o2.mat - o1.mat;
+                    } else {
+                        return p1.eng - p2.eng;
                     }
-                    return o1.eng - o2.eng;
+                } else {
+                    return p2.kor - p1.kor;
                 }
-                return o2.kor - o1.kor;
             }
-        });
+
+            }
+        );
 
         StringBuilder sb = new StringBuilder();
-
-        for(int i=0; i<n; i++) {
-            sb.append(arr[i].name + "\n");
+        for(person p : arr) {
+            sb.append(p.name + "\n");
         }
-
-        System.out.println(sb);
-    }
-
-    // 클래스 객체를 만듬
-    public static class Student {
-        String name;
-        int kor;
-        int eng;
-        int mat;
-
-        public Student(String name, int kor, int eng, int mat) {
-            this.name = name;
-            this.kor = kor;
-            this.eng = eng;
-            this.mat = mat;
-        }
+        System.out.print(sb.toString());
     }
 }
