f3a7a0ae430727f068a889baad0f253ac41ebc24
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-09-05 10:28:57 +0900

    [Gold V] Title: 합분해, Time: 100 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.java
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -3,42 +3,28 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        Long[][] dp = new Long[n+1][k+1];
+        int[][] dp = new int[n+1][k+1];
 
-        // 초기값 설정
-        for(int i=0; i<=n; i++) {
-            dp[i][0] = 0L;
-        }
+        // 초기값
         for(int i=1; i<=n; i++) {
-            dp[i][1] = 1L;
+            dp[i][1] = 1;
         }
+
         for(int i=1; i<=k; i++) {
-            dp[0][i] = 1L;
+            dp[1][i] = i;
         }
 
-        // 점화식
-        for(int i=1; i<=n; i++) {
+        // dp 구하기
+        for(int i=2; i<=n; i++) {
             for(int j=2; j<=k; j++) {
-                dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % 1000000000;
+                dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % 1_000_000_000;
             }
         }
-
-        // dp 출력
-        // for(int i=0; i<=n; i++) {
-        //     for(int j=0; j<=k; j++) {
-        //         System.out.print(dp[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-
         System.out.println(dp[n][k]);
     }
 }
