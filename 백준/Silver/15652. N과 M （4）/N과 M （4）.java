b4163fa22fdfd4b313e55290a94b99445c927ab1
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-11-08 08:34:39 +0900

    [Silver III] Title: N과 M (4), Time: 168 ms, Memory: 18468 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n, m;
+    public static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
+        dfs("", 0, 1);
+        System.out.print(sb.toString());
+    }
+
+    public static void dfs(String str, int depth, int num) {
+        if(depth == m) {
+            sb.append(str + "\n");
+            return;
+        }
+
+        for(int i=num; i<=n; i++) {
+            dfs(str + i + " ", depth+1, i);
+        }
+    }
+}
