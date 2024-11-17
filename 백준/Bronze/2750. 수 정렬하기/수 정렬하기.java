187ac395c70830d4e1befab6a57580c65606fd1c
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2024-11-18 08:12:42 +0900

    [Bronze II] Title: 수 정렬하기, Time: 136 ms, Memory: 16128 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++) {
+            sb.append(arr[i] + "\n");
+        }
+        System.out.print(sb.toString());
+    }
+}
