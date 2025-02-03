47cb1c640b73b19d97f94615fcf64b9d9ce4aa9b
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2025-02-03 12:40:01 +0900

    [Bronze II] Title: 욱제는 효도쟁이야!!, Time: 252 ms, Memory: 21148 KB -BaekjoonHub

---
+++ 백준/Bronze/14487. 욱제는 효도쟁이야！！/욱제는 효도쟁이야！！.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int answer = 0;
+        for(int i=0; i<n-1; i++) {
+            answer += arr[i];
+        }
+        System.out.println(answer);
+    }
+}
