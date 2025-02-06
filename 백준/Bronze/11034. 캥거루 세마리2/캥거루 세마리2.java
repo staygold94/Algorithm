7f164b566d25fce5aeaf9aecde0dfe96055142d4
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2025-02-07 08:45:01 +0900

    [Bronze III] Title: 캥거루 세마리2, Time: 96 ms, Memory: 14008 KB -BaekjoonHub

---
+++ 백준/Bronze/11034. 캥거루 세마리2/캥거루 세마리2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+
+        while((s = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(s);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            System.out.println(Math.max(b-a-1, c-b-1));
+        }
+    }   
+}
