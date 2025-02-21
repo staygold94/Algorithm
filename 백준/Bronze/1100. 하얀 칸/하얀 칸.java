4e4fe33c5ca582c0c07248a4c672395f4a50abcb
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2025-02-21 09:49:44 +0900

    [Bronze II] Title: 하얀 칸, Time: 108 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int answer = 0;
+
+        for(int i=0; i<8; i++) {
+            String s = br.readLine();
+            for(int j=0; j<8; j++) {
+                if((i+j) % 2 == 0 && s.charAt(j) == 'F') {
+                    answer++;
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
