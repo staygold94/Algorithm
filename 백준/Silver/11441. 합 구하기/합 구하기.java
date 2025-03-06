4a459d7ca2c21360bf34f6f78641802248ac69f9
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2025-03-06 10:19:01 +0900

    [Silver III] Title: 합 구하기, Time: 620 ms, Memory: 57312 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] sum = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sum[i] = arr[i];
+            if(i > 0) sum[i] += sum[i-1];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int m = Integer.parseInt(br.readLine());
+        for(int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            sb.append((sum[y] - sum[x] + arr[x]) + "\n");
+        }
+        System.out.print(sb.toString());
+    }
+}
