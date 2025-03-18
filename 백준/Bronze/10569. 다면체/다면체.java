771cb20a1816aa2d500b8655af22c3fd9f8a4d56
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2025-03-18 09:05:21 +0900

    [Bronze III] Title: 다면체, Time: 116 ms, Memory: 14460 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int num = 2 - x + y;
+            sb.append(num + "\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+}
