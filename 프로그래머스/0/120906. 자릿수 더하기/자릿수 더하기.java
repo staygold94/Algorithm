8b3b7d70ef3fd4af7055e2e1e7aa6cede39f535b
Jieun Kim <98931608+staygold94@users.noreply.github.com>
2025-09-18 19:02:38 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,11 +1,10 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        while(n >= 10) {
+        while(n > 0) {
             answer += n%10;
             n /= 10;
         }
-        answer+= n;
         return answer;
     }
 }
